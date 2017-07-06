851a4702f21b96bc2d5f804cc8587b6077cb3dc8
LoyalChen <806030391@qq.com>
2017-07-06 12:34:25 +0800

    Fix the bug when validation can't verify custom ValidationAttribute

--- src/MYMCore/Behavioral/Validation.cs
+++ src/MYMCore/Behavioral/Validation.cs
@@ -10,7 +10,7 @@ namespace MYMCore.Behavioral {
         public static bool IsValidate<T>(T t, out List<ValidationResult> result) {
             var content = new ValidationContext(t);
             result = new List<ValidationResult>();
-            return Validator.TryValidateObject(t, content, result);
+            return Validator.TryValidateObject(t, content, result, true);
         }
 
         public static bool IsValidateProperty<T>(T t, string field, object validateValue, out List<ValidationResult> result) {
