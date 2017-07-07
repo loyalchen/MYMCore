e2ac549c104f01256a0cba64a46159edea5f63c5
LoyalChen <806030391@qq.com>
2017-07-07 18:17:10 +0800

    Add an extension method AppendDelimiter

---
+++ src/MYMCore/Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYMCore.Extensions {
+    public static class StringBuilderExtensions {
+        /// <summary>
+        /// Appends a copy of the specified string to this instance with a delimiter.
+        /// </summary>
+        /// <param name="sb">An instance of <see cref="StringBuilder"/></param>
+        /// <param name="value">The string to append</param>
+        /// <param name="delimiter">Default delimiter "; "</param>
+        /// <returns></returns>
+        public static StringBuilder AppendDelimiter(this StringBuilder sb, string value, string delimiter = default(string)) {
+            if (sb == null) {
+                throw new ArgumentNullException(nameof(sb));
+            }
+
+            delimiter = delimiter == default(string) ? "; " : delimiter;
+
+            if (sb.Length > 0) {
+                sb.Append($"{delimiter}");
+            }
+
+            sb.Append(value);
+
+            return new StringBuilder(sb.ToString());
+        }
+    }
+}
